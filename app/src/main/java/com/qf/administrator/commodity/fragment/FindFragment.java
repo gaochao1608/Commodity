cffd26519fd8e3def81f83e3d5e736fa32b2f774
gggggaochao <gaochao0201@hotmail.com>
2016-11-10 15:48:30 +0800

    firstfrag的test

--- app/src/main/java/com/qf/administrator/commodity/fragment/FindFragment.java
+++ app/src/main/java/com/qf/administrator/commodity/fragment/FindFragment.java
@@ -21,8 +21,7 @@ public class FindFragment extends Fragment {
     private RecyclerView rlv;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_find, container, false);
     }
