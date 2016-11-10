cc06a021095d8da44ee49d714f2b3c004d98d7d8
gggggaochao <gaochao0201@hotmail.com>
2016-11-10 15:47:12 +0800

    firstfrag

--- app/src/main/java/com/qf/administrator/commodity/fragment/FindFragment.java
+++ app/src/main/java/com/qf/administrator/commodity/fragment/FindFragment.java
@@ -2,7 +2,10 @@ package com.qf.administrator.commodity.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,11 +18,7 @@ import com.qf.administrator.commodity.R;
  */
 public class FindFragment extends Fragment {
 
-
-    public FindFragment() {
-        // Required empty public constructor
-    }
-
+    private RecyclerView rlv;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,4 +27,20 @@ public class FindFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_find, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rlv= (RecyclerView) view.findViewById(R.id.rlv_firstfrag);
+        initData();
+        rlv.setLayoutManager(new GridLayoutManager(getContext(),2));
+        initAdapter();
+    }
+
+    private void initAdapter() {
+
+    }
+
+    private void initData() {
+
+    }
 }
