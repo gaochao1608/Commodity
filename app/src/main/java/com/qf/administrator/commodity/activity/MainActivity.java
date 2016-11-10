4734fbba3a0781e4e84ddb43c9d74a94dc64ff69
likainian <1784186573@qq.com>
2016-11-10 15:50:50 +0800

    first

--- app/src/main/java/com/qf/administrator/commodity/activity/MainActivity.java
+++ app/src/main/java/com/qf/administrator/commodity/activity/MainActivity.java
@@ -1,15 +1,36 @@
 package com.qf.administrator.commodity.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 import com.qf.administrator.commodity.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initData();
+        initAdapter();
+    }
+
+    private void initAdapter() {
+
+    }
+
+    private void initData() {
+
+    }
+
+    private void initView() {
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
     }
 }
