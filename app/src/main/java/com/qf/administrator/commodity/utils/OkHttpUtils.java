be445b064fbcab0b5b6f20f6dde86c4811530b48
gggggaochao <gaochao0201@hotmail.com>
2016-11-11 22:30:19 +0800

    找单品列表item的点击事件

--- app/src/main/java/com/qf/administrator/commodity/utils/OkHttpUtils.java
+++ app/src/main/java/com/qf/administrator/commodity/utils/OkHttpUtils.java
@@ -75,7 +75,6 @@ public class OkHttpUtils {
         mClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-
             }
 
             @Override
